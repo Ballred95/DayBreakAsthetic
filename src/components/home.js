ed4c68eb0f6d51fc301ed126fba2472c05753193
Ballred95 <blake.allred95@icloud.com>
2020-08-16 15:10:53 -0600

    Intergrated material UI and testing navbar

--- src/components/home.js
+++ src/components/home.js
@@ -1,9 +1,41 @@
-import React from 'react' 
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import MakeStyles from '@material-ui/styles'
 
-export default function Home(props) {
-    return (
-    <div className=''>
-        <h1>Home</h1>
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+export default function Home() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root} id='navbar'>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            DayBreak Asthetic
+          </Typography>
+          <Button color="inherit">Schedule</Button>
+        </Toolbar>
+      </AppBar>
     </div>
-    )
+  );
 }
