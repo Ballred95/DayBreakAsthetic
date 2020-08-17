181f0f7ff2d5e7d278acc88ace469c5ac37c979e
Ballred95 <blake.allred95@icloud.com>
2020-08-17 15:50:31 -0600

    added image:

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Home from './home';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 export default class App extends Component {
---
+++ src/components/home.js
@@ -0,0 +1,23 @@
+import React from 'react' 
+import {Navbar, Form, FormControl, Button, Nav } from 'react-bootstrap';
+import Logo from './ima'
+
+export default function (props) {
+    return (
+    <div className=''>
+         <Navbar bg="light" variant="light">
+    <Navbar.Brand href="#home"><img src={Logo}></img></Navbar.Brand>
+    <Nav className="mr-auto">
+      <Nav.Link href="#home">Home</Nav.Link>
+      <Nav.Link href="#features">About</Nav.Link>
+      <Nav.Link href="#pricing">Schedule</Nav.Link>
+    </Nav>
+    <Form inline>
+      <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+      <Button variant="outline-info">Search</Button>
+    </Form>
+  </Navbar>
+  
+    </div>
+    )
+}
